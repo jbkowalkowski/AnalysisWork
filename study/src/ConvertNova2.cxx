e71df89819248d16ccb30f71116f209a8cea3dcd
James Kowalkowski <jbk@woof.fnal.gov>
2015-11-12 22:07:55 -0600

    new

--- study/src/ConvertNova2.cxx
+++ study/src/ConvertNova2.cxx
@@ -3,7 +3,10 @@
 #include <vtkProperty.h>
 #include <vtkXMLImageDataWriter.h>
 #include <vtkXMLImageDataReader.h>
+#include <vtkRectilinearGridWriter.h>
+#include <vtkRectilinearGridReader.h>
 #include <vtkImageData.h>
+#include <vtkRectilinearGrid.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 #include <vtkImageDataGeometryFilter.h>
@@ -12,6 +15,7 @@
 #include <vtkPolyDataMapper.h>
 #include <vtkShortArray.h>
 #include <vtkFloatArray.h>
+#include <vtkDoubleArray.h>
 #include <vtkPointData.h>
 #include <vtkCellData.h>
 
@@ -57,6 +61,7 @@ int main(int argc, char *argv[])
 
   std::string filename_in = argv[1];
   std::string filename_out = argv[2];
+  string filename_out_x = filename_out + "_X.vti";
   string filename_out_y = filename_out + "_Y.vti";
 
   // read the raw digits in
@@ -84,66 +89,87 @@ int main(int argc, char *argv[])
     }
 
   // dim_planes_axis
-  size_t dim_xyz = 256;
+  size_t dim_xyz = 257;
 
-  vtkSmartPointer<vtkImageData> imageData_y =
+  vtkSmartPointer<vtkImageData> grid_x =
+    vtkSmartPointer<vtkImageData>::New();
+
+  vtkSmartPointer<vtkImageData> grid_y =
     vtkSmartPointer<vtkImageData>::New();
 
   // only two dimensions: (whick sample, which_wire)
-  imageData_y->SetDimensions(dim_xyz,dim_xyz,dim_xyz);
-  imageData_y->AllocateScalars(VTK_FLOAT, 1);
-
-  int* dims = imageData_y->GetDimensions();
-  for(int x=0;x<dims[0];++x)
-    for(int y=0;y<dims[1];++y)
-      for(int z=0;z<dims[2];++z)
-      {
-	  float* pixel_y=
-	    static_cast<float*>(imageData_y->GetScalarPointer(x,y,z));
-	  pixel_y[0] = 0.0;
-	  // pixel_y[1] = 0.0;
-      }
-
-  auto func = [&](int x,int y,int z, float e) 
-  { 
-    float* pixel_y=
-      static_cast<float*>(imageData_y->GetScalarPointer(x,y,z));
-    pixel_y[0]+=e;
-  };
-
-  for(auto& it:wfs)
-  {
-    auto x_max = std::min(it.x+it.x_err,255.0f);
-    auto y_max = std::min(it.y+it.y_err,255.0f);
-    auto z_max = std::min(it.z+5.5f,255.0f);
-    for(size_t ex=it.x-it.x_err;ex<x_max;++ex)
-     for(size_t ey=it.y-it.y_err;ey<y_max;++ey)
-      for(size_t ez=it.z-5.5f;ez<z_max;++ez) 
-        func(ex,ey,ez,it.energy+1.0);
-  }
-
-#if 0
-  // auto cell_data = imageData_y->GetCellData();
-
-  auto p_cells = imageData_y->GetCellData();
-  auto p_points = imageData_y->GetPointData();
-
-  // there is an attribute called ImageScalars on the point data
-  cerr << "# cells = " << imageData_y->GetNumberOfCells() << "\n";
-  cerr << "# points = " << imageData_y->GetNumberOfPoints() << "\n";
- 
-  // example of getting the scalar data as a float array
-  // vtkFloatArray* sa = vtkFloatArray::SafeDownCast(p_scalers);
-
+  grid_x->SetDimensions(dim_xyz,2,dim_xyz);
+  grid_y->SetDimensions(2,dim_xyz,dim_xyz);
   
+  // (i,j,k) idx_flat=k*(nptsx*nptsy)+j*nptrx+i
+
+  vtkSmartPointer<vtkFloatArray> energy_x =
+    vtkSmartPointer<vtkFloatArray>::New();
+  energy_x->SetName("energy");
+  energy_x->Resize(256*256);
+  energy_x->SetNumberOfValues(256*256);
+
+  vtkSmartPointer<vtkFloatArray> energy_y = 
+    vtkSmartPointer<vtkFloatArray>::New();
+  energy_y->SetName("energy");
+  energy_y->Resize(256*256);
+  energy_y->SetNumberOfValues(256*256);
+
+#if 1
+  auto flat_index_y = [&](int i,int j, int k)
+    {
+      auto a = k*(1*256)+j*1+i;
+      cout << "y index " << a << "\n";
+      return a;
+    };
+  auto flat_index_x = [&](int i,int j, int k)
+    {
+      auto a = k*(256*1)+j*256+i;
+      cout << "x index " << a << "\n";
+      return a;
+    };
+
+  for(size_t i=0;i<wfs.size();++i)
+    {
+      float tmp = wfs[i].energy+1.0;
+      if(i%2==1)
+	{
+	  auto u=flat_index_x(wfs[i].x,0,wfs[i].z);
+	  energy_x->SetTupleValue(u,&tmp);
+	  auto v=flat_index_x(wfs[i].x,1,wfs[i].z);
+	  energy_x->SetTupleValue(v,&tmp);
+	}
+      else
+	{
+	  auto u=flat_index_y(0,wfs[i].y,wfs[i].z);
+	  energy_y->SetTupleValue(u,&tmp);
+	  auto v=flat_index_y(1,wfs[i].y,wfs[i].z);
+	  energy_y->SetTupleValue(v,&tmp);
+	}
+    }
+
+  cout << "y cell type = " << grid_x->GetCellType(flat_index_y(1,4,5)) << "\n";
+  cout << "x cell type = " << grid_y->GetCellType(flat_index_x(1,4,5)) << "\n";
 #endif
-  
 
+  auto x_cells = grid_x->GetCellData();
+  auto y_cells = grid_y->GetCellData();
+
+  x_cells->AddArray(energy_x);
+  y_cells->AddArray(energy_y);
+
+  std::cout << "Points: " << grid_x->GetNumberOfPoints() << "\n";
+  std::cout << "Cells: " << grid_x->GetNumberOfCells() << "\n";
+  
   vtkSmartPointer<vtkXMLImageDataWriter> writer =
     vtkSmartPointer<vtkXMLImageDataWriter>::New();
 
+  writer->SetFileName(filename_out_x.c_str());
+  writer->SetInputData(grid_x);
+  writer->Write();
+
   writer->SetFileName(filename_out_y.c_str());
-  writer->SetInputData(imageData_y);
+  writer->SetInputData(grid_y);
   writer->Write();
   return 0;
 }
