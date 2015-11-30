951ae06a9ee2f2474a327b6903043977786e0a6e
Jim Kowalkowski <jbk@fnal.gov>
2015-11-29 23:25:55 -0600

    new polydata writer

--- study/src/ConvertWaveform2.cxx
+++ study/src/ConvertWaveform2.cxx
@@ -4,12 +4,13 @@
 #include <vtkXMLImageDataWriter.h>
 #include <vtkXMLImageDataReader.h>
 #include <vtkImageData.h>
+#include <vtkPolyData.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 #include <vtkImageDataGeometryFilter.h>
 #include <vtkRenderWindow.h>
 #include <vtkActor.h>
-#include <vtkPolyDataMapper.h>
+#include <vtkPolyDataWriter.h>
 #include <vtkShortArray.h>
 #include <vtkPointData.h>
 #include <vtkCellData.h>
@@ -61,6 +62,51 @@ struct Waveform
   short high,low;
 };
 typedef vector<Waveform> Waveforms;
+
+void gen_points(vtkSmartPointer<vtkXMLPolyDataWriter>& writer,
+		std::string const& filename,
+		Waveform* wfsp, size_t dim_S, size_t dim)
+{
+  
+  // Positions and cells
+  vtkSmartPointer<vtkPoints> pts =
+    vtkSmartPointer<vtkPoints>::New();
+  vtkSmartPointer<vtkCellArray> verts =
+    vtkSmartPointer<vtkCellArray>::New();
+  vtkSmartPointer<vtkShortArray> energy =
+    vtkSmartPointer<vtkShortArray>::New();
+
+  energy->SetName("energy");
+  energy->Resize(dim*dim_S);
+  energy->SetNumberOfValues(dim*dim_S);
+
+  for (size_t wire = 0; wire < dim;++wire)
+    {
+      Waveform& wf_u = *(wfsp[wire]);
+
+      for (size_t sample = 0; sample < dim_S;++sample)
+	{
+	  short au = wf_u.adcs[sample];
+	  pts->InsertNextPoint(wire,sample,0);
+	  vtkIdType pid[1];
+	  verts->InsertNextCell(1,pid);
+	  energy->InsertNextValue(au);	    
+	}
+    }
+
+  // Create a polydata
+  vtkSmartPointer<vtkPolyData> polydata = 
+    vtkSmartPointer<vtkPolyData>::New();
+  polydata->SetPoints ( pts );
+  polydata->SetVerts ( verts );
+  polydata->GetPointData()->AddArray(energy);
+
+  writer->SetFileName(filename.c_str());
+  writer->SetInputData(polydata);
+  writer->Write();
+
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -120,14 +166,6 @@ and V planes to be collected by the Y plane.
 
   vector<Waveform*> wfsp;
   for(size_t i=0;i<wfs.size();++i) { wfsp.push_back(&wfs[i]); }
-#if 0
-  sort(&wfsp[0],&wfsp[2400],
-       [](Waveform* a, Waveform* b){return a->channel<b->channel;});
-  sort(&wfsp[2400],&wfsp[2400+2400],
-       [](Waveform* a, Waveform* b){return a->channel<b->channel;});
-  sort(&wfsp[2400+2400],&wfsp[wfsp.size()],
-       [](Waveform* a, Waveform* b){return a->channel<b->channel;});
-#else
   sort(wfsp.begin(), wfsp.end(),
        [](Waveform* a, Waveform* b)
         {return (a->plane < b->plane)?true:
@@ -135,197 +173,18 @@ and V planes to be collected by the Y plane.
              (a->wireindex < b->wireindex)?true:
                false;}
        );
-#endif
 
   // dim_planes_axis
   size_t dim_UV = 2400, dim_Y = 3456;
   size_t dim_S = wfs[0].adcs.size(); // /4;
 
-  vtkSmartPointer<vtkImageData> imageData_u =
-    vtkSmartPointer<vtkImageData>::New();
-  vtkSmartPointer<vtkImageData> imageData_v =
-    vtkSmartPointer<vtkImageData>::New();
-  vtkSmartPointer<vtkImageData> imageData_y =
-    vtkSmartPointer<vtkImageData>::New();
-
-  // only two dimensions: (whick sample, which_wire)
-  imageData_u->SetDimensions(dim_UV,dim_S,1);
-  imageData_v->SetDimensions(dim_UV,dim_S,1);
-  imageData_y->SetDimensions(dim_Y,dim_S,1);
-  //imageData_u->SetDimensions(dim_UV+1,dim_S+1,2);
-  //imageData_v->SetDimensions(dim_UV+1,dim_S+1,2);
-  //imageData_y->SetDimensions(dim_Y+1,dim_S+1,2);
-
-  vtkSmartPointer<vtkShortArray> energy_u =
-    vtkSmartPointer<vtkShortArray>::New();
-  energy_u->SetName("energy");
-  energy_u->Resize(dim_UV*dim_S);
-  energy_u->SetNumberOfValues(dim_UV*dim_S);
-
-  vtkSmartPointer<vtkShortArray> energy_v = 
-    vtkSmartPointer<vtkShortArray>::New();
-  energy_v->SetName("energy");
-  energy_v->Resize(dim_UV*dim_S);
-  energy_v->SetNumberOfValues(dim_UV*dim_S);
-
-  vtkSmartPointer<vtkShortArray> energy_y = 
-    vtkSmartPointer<vtkShortArray>::New();
-  energy_y->SetName("energy");
-  energy_y->Resize(dim_Y*dim_S);
-  energy_y->SetNumberOfValues(dim_Y*dim_S);
-
-  // (i,j,k) idx_flat=k*(nptsx*nptsy)+j*nptrx+i
-
-#if 1
-  // ------------ 
-  // calculate median over each plane
-
-  const double get_rid = .49;
-  std::vector<short> all_plane;
-  all_plane.reserve(3500*10000);
-  cout << "generating median" << std::endl;
-
-  for(size_t wire=0;wire<dim_UV;++wire)
-  {
-      Waveform& wfa = *(wfsp[wire]);
-      for(size_t sample=0;sample<dim_S;++sample)
-        all_plane.push_back(wfa.adcs[sample]);
-  }
-
-  sort(all_plane.begin(),all_plane.end());
-  auto all_half_u = all_plane.size()/2;
-  auto all_median_u = all_plane[all_half_u];
-  size_t all_dist_u = all_plane.size() * get_rid;
-  auto all_high_u = all_plane[all_half_u+all_dist_u]-all_median_u;
-  auto all_low_u = all_plane[all_half_u-all_dist_u]-all_median_u;
-  all_plane.clear();
- cerr << all_half_u << " " << all_median_u << " " << all_dist_u << " " << all_high_u << " " << all_low_u << "\n";
-
-  for(size_t wire=0;wire<dim_UV;++wire)
-  {
-      Waveform& wfb = *(wfsp[wire+dim_UV]);
-      for(size_t sample=0;sample<dim_S;++sample)
-        all_plane.push_back(wfb.adcs[sample]);
-  }
-
-  sort(all_plane.begin(),all_plane.end());
-  auto all_half_v = all_plane.size()/2;
-  auto all_median_v = all_plane[all_half_v];
-  size_t all_dist_v = all_plane.size() * get_rid;
-  auto all_high_v = all_plane[all_half_v+all_dist_v]-all_median_v;
-  auto all_low_v = all_plane[all_half_v-all_dist_v]-all_median_v;
-  all_plane.clear();
-
-  for(size_t wire=0;wire<dim_Y;++wire)
-    {
-      Waveform& wf_y = *(wfsp[wire+dim_UV*2]);
-
-      for (size_t sample = 0; sample < dim_S;++sample)
-	{
-	  all_plane.push_back(wf_y.adcs[sample]);
-        }
-    }
-
-  sort(all_plane.begin(),all_plane.end());
-  auto all_half_y = all_plane.size()/2;
-  auto all_median_y = all_plane[all_half_y];
-  size_t all_dist_y = all_plane.size() * get_rid;
-  auto all_high_y = all_plane[all_half_y+all_dist_y]-all_median_y;
-  auto all_low_y = all_plane[all_half_y-all_dist_y]-all_median_y;
-  all_plane.clear();
-
-  cout << "generating median done." << std::endl;
-  // ------------
-#endif
-
-  auto flat_index_uv = [&](int i,int j, int k)
-    {
-      auto a = k*(dim_UV*dim_S)+j*dim_UV+i;
-      //cout << "y index " << a << "\n";
-      return a;
-    };
-  auto flat_index_y = [&](int i,int j, int k)
-    {
-      auto a = k*(dim_Y*dim_S)+j*dim_Y+i;
-      //cout << "x index " << a << "\n";
-      return a;
-    };
+  vtkSmartPointer<vtkXMLPolyDataWriter> writer =
+    vtkSmartPointer<vtkXMLPolyDataWriter>::New();
   
-  int* dims = imageData_u->GetDimensions();
-  cout << "dim uv = " << dims[0] << " " << dims[1] << " " << dims[2] << "\n";
+  // first u, then v, then y
+  gen_points(writer, filename_out_u, &wfsp[0], dim_S, dim_UV);
+  gen_points(writer, filename_out_v, &wfsp[dim_UV], dim_S, dim_UV);
+  gen_points(writer, filename_out_y, &wfsp[dim_UV*2], dim_S, dim_Y);
 
-  for (size_t wire = 0; wire < dim_UV;++wire)
-    {
-      Waveform& wf_u = *(wfsp[wire]);
-      Waveform& wf_v = *(wfsp[wire+dim_UV]);
-
-      for (size_t sample = 0; sample < dim_S;++sample)
-	{
-	  // short au = wf_u.adcs[sample] - wf_u.median;
-	  // short av = wf_v.adcs[sample] - wf_v.median;
-	  // au=(au<wf_u.high && au>wf_u.low)?0:au;
-	  // av=(av<wf_v.high && av>wf_v.low)?0:av;
-	  short au = wf_u.adcs[sample] - all_median_u;
-	  short av = wf_v.adcs[sample] - all_median_v;
-	  au=(au<all_high_u && au>all_low_u)?0:au;
-	  av=(av<all_high_v && av>all_low_v)?0:av;
-	  
-	  // auto u = flat_index_uv(wire,sample,0);
-	  // energy_u->SetTupleValue(u,&au);
-	  // auto v = flat_index_uv(wire,sample,0);
-	  // energy_v->SetTupleValue(v,&av);
-	  auto u = flat_index_uv(wire,sample,0);
-	  energy_u->SetTupleValue(u,&au);
-	  auto v = flat_index_uv(wire,sample,0);
-	  energy_v->SetTupleValue(v,&av);
-      }
-    }
-
-  dims = imageData_y->GetDimensions();
-  cout << "dim y = " << dims[0] << " " << dims[1] << " " << dims[2] << "\n";
-
-  for(size_t wire=0;wire<dim_Y;++wire)
-    {
-      Waveform& wf_y = *(wfsp[wire+dim_UV*2]);
-
-      for (size_t sample = 0; sample < dim_S;++sample)
-	{
-	  // short ay = wf_y.adcs[sample] - wf_y.median;
-	  // ay=(ay<wf_y.high && ay>wf_y.low)?0:ay;
-	  short ay = wf_y.adcs[sample] - all_median_y;
-	  ay=(ay<all_high_y && ay>all_low_y)?0:ay;
-
-	  auto y = flat_index_y(wire,sample,0);
-	  energy_y->SetTupleValue(y,&ay);
-      }
-    }
-
-  auto u_cells = imageData_u->GetPointData();
-  auto v_cells = imageData_v->GetPointData();
-  auto y_cells = imageData_y->GetPointData();
-
-  u_cells->AddArray(energy_u);
-  v_cells->AddArray(energy_v);
-  y_cells->AddArray(energy_y);
-
-  std::cout << "u Points: " << imageData_u->GetNumberOfPoints() << "\n";
-  std::cout << "u Cells: " << imageData_u->GetNumberOfCells() << "\n";
-  std::cout << "y Points: " << imageData_y->GetNumberOfPoints() << "\n";
-  std::cout << "y Cells: " << imageData_y->GetNumberOfCells() << "\n";
-  
-  vtkSmartPointer<vtkXMLImageDataWriter> writer =
-    vtkSmartPointer<vtkXMLImageDataWriter>::New();
-
-  writer->SetFileName(filename_out_u.c_str());
-  writer->SetInputData(imageData_u);
-  writer->Write();
-
-  writer->SetFileName(filename_out_v.c_str());
-  writer->SetInputData(imageData_v);
-  writer->Write();
-
-  writer->SetFileName(filename_out_y.c_str());
-  writer->SetInputData(imageData_y);
-  writer->Write();
   return 0;
 }
