ef5936d3f9f58c07dc993b8bb7d881cb2c16c119
James_Kowalkowski <jbk@fnal.gov>
2015-12-08 17:38:12 -0600

    checks for build dir

--- study/setup.sh
+++ study/setup.sh
@@ -3,6 +3,14 @@ setup uboonecode v04_26_03_01 -q e7:prof
 setup cetbuildtools v4_16_00
 # setup mrb v1_05_03
 
+if [ -d build ]
+then
+echo "build directory already exists"
+else
+mkdir build
+echo "made build directory"
+fi
+
 INSTALL_PATH=/uboone/app/users/jbk/install
 LIB_PATH=${INSTALL_PATH}/lib
 INC_PATH=${INSTALL_PATH}/include
@@ -17,3 +25,6 @@ else
 fi
 
 PATH=${PATH}:${BIN_PATH}
+
+cd build
+source ../src/ups/setup_for_development -p
