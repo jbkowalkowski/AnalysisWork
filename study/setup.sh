080a805564177b28b3a45ff94ce1307248cce08e
James_Kowalkowski <jbk@fnal.gov>
2015-12-08 17:00:59 -0600

    for uBoonegpvm machine use

--- study/setup.sh
+++ study/setup.sh
@@ -1,4 +1,19 @@
 
-setup uboonecode v04_21_00 -q e7:prof
+setup uboonecode v04_26_03_01 -q e7:prof
 setup cetbuildtools v4_16_00
 # setup mrb v1_05_03
+
+INSTALL_PATH=/uboone/app/users/jbk/install
+LIB_PATH=${INSTALL_PATH}/lib
+INC_PATH=${INSTALL_PATH}/include
+BIN_PATH=${INSTALL_PATH}/bin
+
+if [ -z "$LD_LIBRARY_PATH" ]
+then
+ # is zero length
+ export LD_LIBRARY_PATH=$LIB_PATH
+else
+ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$LIB_PATH
+fi
+
+PATH=${PATH}:${BIN_PATH}
