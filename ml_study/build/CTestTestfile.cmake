85881b5cea768c042cad45b417fd33be5e7eae13
Jim Kowalkowski <jbk@fnal.gov>
2016-06-09 23:28:51 -0500

    oops

--- ml_study/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jbk/checkouts/AnalysisWork/ml_study/src
-# Build directory: /home/jbk/checkouts/AnalysisWork/ml_study/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
